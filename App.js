97a7e86dd057b25b12e6819a12f4fcac38d5dfec
Dishant <dishant0170@gmail.com>
2021-12-22 01:53:44 -0500

    signing UI complete

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function App() {
+import Home from "./src/screens/Home";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            options={{ headerShown: false }}
+            component={Home}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/screens/Home.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
+const Home = () => {
+  let [fontsLoaded] = useFonts({
+    "Lobster-Regular": require("../../assets/fonts/Lobster-Regular.ttf"),
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.logo}>Fresh Grocer</Text>
+        <View style={styles.loginFormContainer}>
+          <Text style={styles.text}>Welcome!</Text>
+          <TextInput style={styles.input} placeholder="Email" />
+          <TextInput style={styles.input} placeholder="Password" />
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+          <Text style={styles.forgotPassword}>Forgot Password?</Text>
+          <Text
+            style={{ color: "#a3a4a5", alignSelf: "center", marginTop: "10%" }}
+          >
+            Don't have an account?
+            <Text style={{ color: "#ff5b2d" }}> Sign Up</Text>
+          </Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "white",
+  },
+  logo: {
+    color: "#ff5b2d",
+    fontSize: 28,
+    fontFamily: "Lobster-Regular",
+    alignSelf: "center",
+  },
+  loginFormContainer: {
+    top: "10%",
+    paddingHorizontal: "5%",
+  },
+  text: {
+    color: "#8d8e8f",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  input: {
+    backgroundColor: "#ecedf1",
+    height: 45,
+    marginBottom: 20,
+    borderRadius: 15,
+    shadowColor: "rgba(0,0,0, .4)", // IOS
+    shadowOffset: { height: 1, width: 1 }, // IOS
+    shadowOpacity: 1, // IOS
+    shadowRadius: 1, //IOS
+    elevation: 2, // Android
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: "#ff5b2d",
+    borderRadius: 15,
+    shadowColor: "rgba(0,0,0, .4)", // IOS
+    shadowOffset: { height: 1, width: 1 }, // IOS
+    shadowOpacity: 1, // IOS
+    shadowRadius: 1, //IOS
+    elevation: 2, // Android
+    height: 45,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    alignSelf: "center",
+  },
+  forgotPassword: {
+    color: "#ff5b2d",
+    alignSelf: "center",
+    marginTop: "20%",
+  },
+});
+
+export default Home;
