9cdec901bfe5cda00dbc31d91315577022dd8695
Dishant <dishant0170@gmail.com>
2021-12-27 18:15:59 -0500

    Home page design complete

---
+++ src/components/User/CategoryItem.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { StyleSheet, View, Image, Text, TouchableOpacity } from "react-native";
+
+const CategoryItem = ({ category }) => {
+  return (
+    <View>
+      <TouchableOpacity>
+        <View style={styles.categoryBox}>
+          <Image style={styles.categoryThumb} source={category.thumb} />
+          <Text style={styles.categoryTitle}>{category.title}</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  categoryBox: {
+    borderWidth: 1,
+    borderColor: "#BDB9B7",
+    borderRadius: 13,
+    width: 165,
+    height: 185,
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  categoryThumb: {
+    borderRadius: 15,
+    width: 116,
+    height: 116,
+    marginTop: 10,
+  },
+  categoryTitle: {
+    fontSize: 14,
+    lineHeight: 16,
+    fontWeight: "bold",
+    color: "#9D9998",
+  },
+});
+export default CategoryItem;
